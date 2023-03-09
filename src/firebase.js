58d49cdf014003ac6ba0d50c56d2676c4daf673e
Muhammed Ekici <muhammedkasim.ekici@outlook.com>
2023-03-09 19:08:23 +0100

    init

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+const firebaseConfig = {
+  apiKey: "AIzaSyAbftVRa0JSj1KD5awZ71n3pFSWj51JuZw",
+  authDomain: "comair-b584e.firebaseapp.com",
+  projectId: "comair-b584e",
+  storageBucket: "comair-b584e.appspot.com",
+  messagingSenderId: "921614304419",
+  appId: "1:921614304419:web:921f58a111d051db4eeb16",
+};
+
+firebase.initializeApp(firebaseConfig);
+export { firebase };
